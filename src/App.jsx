e9878506cd9092e227a97962454c60090c8bb76d
sayed132 <rafsansayed132@gmail.com>
2024-03-11 19:14:51 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Route";
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Router/Route.jsx
@@ -0,0 +1,15 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+
+const router = createBrowserRouter([
+{
+    path: "/",
+    element: <MainLayout/>,
+    children:[
+
+    ]
+}
+])
+
+export default router;
